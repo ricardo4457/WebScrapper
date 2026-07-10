df088299e7b5f01d3313e0c73f5c6a2dc2dc143b
ricardo4457 <ricardo.a.vieira123@gmail.com>
2026-07-10 21:03:42 +0100

    refactor : fix folder places

--- src/routes/scrape.js
+++ src/routes/scrape.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const scrapeQueue = require("../queues/ScrapeQueue");
+const scrapeQueue = require("/queue/ScrapeQueue");
 
 
 router.post("/scrape/run", async (req, res) => {
