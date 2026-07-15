aa7747cb002bfca949f19841df7c37469b3052b1
ricardo4457 <ricardo.a.vieira123@gmail.com>
2026-07-15 15:14:00 +0100

    feat: ajustar processamento de filas e definição do scraper job

--- src/queue/ScrapeQueue.js
+++ src/queue/ScrapeQueue.js
@@ -1,35 +1,33 @@
 const { Queue } = require("bullmq");
 const redis = require("../config/redis");
 
-
 class ScrapeQueue {
-
     constructor() {
-        this.queue = new Queue(
-            "book-scraper",
-            {
-                connection: redis,
-            }
-        );
+        this.queue = new Queue("book-scraper", {
+            connection: redis,
+        });
     }
 
+    /**
+     * Adds a scraping task to the queue with custom BullMQ options.
+     * @param {Object} data - The payload containing url and runId.
+     * @param {Object} [options={}] - Optional BullMQ job options (e.g., retries, cleanup).
+     * @returns {Promise<Job>} The created BullMQ Job instance.
+     */
 
-    async add(data) {
-
-        return await this.queue.add(
-            "run",
-            data
-        );
-
+    async add(data, options = {}) {
+        return await this.queue.add("run", data, options);
     }
 
-
+    /**
+     * Finds a job by its unique ID.
+     * @param {string} id - The job ID.
+     * @returns {Promise<Job|null>}
+     */
+    
     async find(id) {
-
         return await this.queue.getJob(id);
-
     }
 }
-
 
 module.exports = new ScrapeQueue();
