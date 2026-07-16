3304d94234e4fc34768349bd093b1640e3235b1e
ricardo4457 <ricardo.a.vieira123@gmail.com>
2026-07-16 19:34:11 +0100

    refactor: remove coments

--- src/queue/ScrapeQueue.js
+++ src/queue/ScrapeQueue.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const { Queue } = require("bullmq");
 const redis = require("../config/redis");
 
@@ -8,23 +10,14 @@ class ScrapeQueue {
         });
     }
 
-    /**
-     * Adds a scraping task to the queue with custom BullMQ options.
-     * @param {Object} data - The payload containing url and runId.
-     * @param {Object} [options={}] - Optional BullMQ job options (e.g., retries, cleanup).
-     * @returns {Promise<Job>} The created BullMQ Job instance.
-     */
 
-    async add(data, options = {}) {
-        return await this.queue.add("run", data, options);
+    async add(name, data, options = {}) {
+        return await this.queue.add(name, data, options);
     }
 
     /**
      * Finds a job by its unique ID.
-     * @param {string} id - The job ID.
-     * @returns {Promise<Job|null>}
      */
-    
     async find(id) {
         return await this.queue.getJob(id);
     }
