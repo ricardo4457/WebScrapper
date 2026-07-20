6e3babf1a8ca7c1c301d26c20badd2646c641ff7
ricardo4457 <ricardo.a.vieira123@gmail.com>
2026-07-20 19:11:05 +0100

    fix: correct case-sensitive require path for Mapnavigation module

--- src/scrapper/scraper.js
+++ src/scrapper/scraper.js
@@ -1,18 +1,18 @@
 "use strict";
 
 const comboNavigation = require("./navigation/comboNavigation");
-const mapNavigation = require("./navigation/mapNavigation");
+const mapNavigation = require("./navigation/Mapnavigation");
 const subjects = require("./subjects");
 const books = require("./books");
 const { mergeExclusive } = require("../utils/MergeExclusive");
 
-const modules = { comboNavigation, mapNavigation, subjects, books };
+const modules = { comboNavigation, Mapnavigation, subjects, books };
 
 mergeExclusive(modules);
 
 module.exports = {
   ...comboNavigation,
-  ...mapNavigation,
+  ...Mapnavigation,
   ...subjects,
   ...books,
 };
