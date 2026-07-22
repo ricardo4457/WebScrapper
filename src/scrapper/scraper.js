1a62527a3e8fa03c4847b872a33f730a10285de8
ricardo4457 <ricardo.a.vieira123@gmail.com>
2026-07-22 20:15:04 +0100

    feat : rollback function after doing first job stores refetches data

--- src/scrapper/scraper.js
+++ src/scrapper/scraper.js
@@ -1,18 +1,43 @@
 "use strict";
 
 const comboNavigation = require("./navigation/comboNavigation");
+const schoolNavigation = require("./navigation/schoolNavigation");
 const mapNavigation = require("./navigation/Mapnavigation");
 const subjects = require("./subjects");
 const books = require("./books");
 const { mergeExclusive } = require("../utils/MergeExclusive");
 
-const modules = { comboNavigation, mapNavigation, subjects, books };
+const modules = {
+  comboNavigation,
+  mapNavigation,
+  subjects,
+  books,
+  schoolNavigation,
+};
 
 mergeExclusive(modules);
+
+/**
+ * Selects the year, cycle, district and city.
+ * Reused for schools in the same city to avoid repeating the same steps.
+ */
+async function navigateToLocation(
+  page,
+  { year, teaching_cycle, district, city },
+) {
+  await comboNavigation.selectYearAndCycle(page, {
+    yearLabel: year,
+    teachingType: teaching_cycle,
+  });
+  await comboNavigation.selectDistrict(page, district);
+  await comboNavigation.selectCity(page, city);
+}
 
 module.exports = {
   ...comboNavigation,
   ...mapNavigation,
   ...subjects,
   ...books,
+  ...schoolNavigation,
+  navigateToLocation,
 };
